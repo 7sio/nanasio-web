c524a752135790eb33103c31f3b62b1580aa3572
nanasio <7sio.80k@gmail.com>
2025-08-05 00:27:33 +0900

    プロフィール編集

--- app/data/profileData.ts
+++ app/data/profileData.ts
@@ -7,7 +7,7 @@ export const profile = {
     hobbies: ["ホラー・伝奇作品鑑賞", "写真撮影", "ワールド探索", "お絵かき"],
     features: ["白・銀髪", "赤目"],
     favoriteFoods: ["モンブラン", "寿司"],
-    favoriteGames: ["ゼノブレイド2", "スプラトゥーン2/3", "ひぐらしのなく頃に", "Omegaの視界"],
+    favoriteGames: ["どうぶつの森シリーズ", "ゼノブレイド2", "スプラトゥーンシリーズ", "ひぐらしのなく頃に", "Omegaの視界", "BlueArchive"],
     favoriteThings: ["青い空", "青い海", "洗礼されたシンプル"],
     strengths: ["戦略性", "着想", "達成欲", "収集心", "個別化"],
     image: "/profile-nanasio.webp"
